9c6cbe61050e9774858d3ce6ea88b6fd3421e564
MichaelIllescas <joni.illes@hotmail.com>
2025-08-27 05:28:38 -0300

    Renombrada interfaz UpdateUserUseCase a UpdateUserUsecase y agregado import

--- BackendPublicacionWeb/publicacionWeb/src/main/java/com/example/backend/users/service/usecase/UpdateUserUsecase.java
+++ BackendPublicacionWeb/publicacionWeb/src/main/java/com/example/backend/users/service/usecase/UpdateUserUsecase.java
@@ -4,5 +4,6 @@ import com.example.backend.users.controller.dto.UpdateLoggedUserDTO;
 import com.example.backend.users.controller.dto.UserRequestDTO;
 
 public interface UpdateUserUsecase {
+
     void execute (Long id, UserRequestDTO userRequest);
 }
